868cfaaefe9b0aec0f16058a4d1890d46be8ea53
mixershpixer <max.grechuha@mail.ru>
2019-02-13 21:11:10 +0300

    LAST

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const axios = require('axios');
+const http = require('http');
+
+async function main()
+{
+    const app = express();
+    const server = http.createServer(app);
+    const io = require('socket.io')(server);
+
+    app.use(require('body-parser').urlencoded({extended: true}));
+    app.use(express.static('public'));
+
+    io.sockets.on('connection', async socket =>
+    {
+        let i = 1;
+        let currency = "USD";
+        let nowInterval = null;
+        let time = 10000000;
+        let mySock = socket;
+        console.log(socket.id);
+        socket.on('stop', (data) =>
+        {
+            clearInterval(nowInterval);
+        });
+
+        socket.on('currency', async function(data)
+        {
+            console.log(data);
+            data.time < 1000 ? data.time = 1000 : null;
+
+            currency = data.curr;
+            time = data.time;
+
+            nowInterval = setInterval(async () =>
+            {
+                socket.emit('btc', (await axios.get('https://blockchain.info/en/ticker')).data[currency])
+            }
+                , time);
+        });
+
+        socket.on('getBuy', async (msg) =>
+        {
+            console.log("selling: "+msg);
+            io.emit('buy', msg);
+        });
+
+        socket.on('getSell', async (msg) =>
+        {
+            console.log("buying: "+msg);
+            io.emit('sell', msg);
+        });
+    });
+
+    server.listen(3010, ()=>
+    {
+        console.log('Example app listening on http://localhost:3010!');
+    });
+}
+main();
---
+++ worker.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+class Worker {
+  constructor(timer = 1000) {
+    this.interval = timer;
+    this.data = {};
+    this.working();
+  }
+
+  setTimer(timer) {
+    this.timer = timer;
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  async working() {
+    try {
+      let response = await axios.get("https://blockchain.info/en/ticker");
+      this.data = response.data;
+
+    setTimeout(() => 
+    { 
+      this.working(); 
+    }, this.timer);
+    } 
+    catch (e) 
+    {
+      console.error(e);
+    }
+  }
+}
+
+module.exports = Worker;
